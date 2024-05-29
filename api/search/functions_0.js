9a70491041b66c098a4f318073677fa4fd2db326
GitHub Action <action@github.com>
2024-05-29 11:16:14 +0000

    Update documentation

--- api/search/functions_0.js
+++ api/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['activate_117',['activate',['../classethercat__interface_1_1EcMaster.html#a35b27abdf7f794b5cdd5c0414c880ea1',1,'ethercat_interface::EcMaster']]],
+  ['activate_117',['activate',['../classethercat__interface_1_1EcMaster.html#afb94d3d0b2c98fc5d94bea7f682d735d',1,'ethercat_interface::EcMaster']]],
   ['addslave_118',['addSlave',['../classethercat__interface_1_1EcMaster.html#aeda98e7ea5c02550192bb3d07f352279',1,'ethercat_interface::EcMaster']]],
   ['assign_5factivate_5fdc_5fsync_119',['assign_activate_dc_sync',['../classethercat__interface_1_1EcSlave.html#a10142d556f0e541d7ff2133a9d0bfbb1',1,'ethercat_interface::EcSlave']]]
 ];
