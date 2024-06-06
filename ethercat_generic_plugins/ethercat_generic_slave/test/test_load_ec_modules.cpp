b90c92158b81c7bf02c3e7216c3d7b6e097a04f3
Manuel YGUEL <manuel.yguel@gmail.com>
2024-06-06 17:27:56 +0200

    Fix CMakeLists.txt and the format of one cpp file to make all tests pass.

--- ethercat_generic_plugins/ethercat_generic_slave/test/test_load_ec_modules.cpp
+++ ethercat_generic_plugins/ethercat_generic_slave/test/test_load_ec_modules.cpp
@@ -20,7 +20,7 @@
 
 TEST(TestLoadGenericEcSlave, load_ec_module)
 {
-  pluginlib::ClassLoader<ethercat_interface::EcSlave> ec_loader_{
-    "ethercat_interface", "ethercat_interface::EcSlave"};
+  pluginlib::ClassLoader<ethercat_interface::EcSlave> ec_loader_(
+    "ethercat_interface", "ethercat_interface::EcSlave");
   ASSERT_NO_THROW(ec_loader_.createSharedInstance("ethercat_generic_plugins/GenericEcSlave"));
 }
