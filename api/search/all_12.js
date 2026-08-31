4909921121de3cd2fe47d64cc88c1b75ed60f20b
GitHub Action <action@github.com>
2026-08-31 14:24:02 +0000

    Update documentation

--- api/search/all_12.js
+++ api/search/all_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['updatestate_0',['updateState',['../classethercat__interface_1_1EcSlaveBase.html#ac7cf7bc47953321ba26bcc9130d263a2',1,'ethercat_interface::EcSlaveBase']]],
-  ['upload_5fslave_5fsdo_1',['upload_slave_sdo',['../classethercat__interface_1_1EcMasterBase.html#ae4c677283ba337c29a227cad90a00e68',1,'ethercat_interface::EcMasterBase']]]
+  ['upload_5fslave_5fsdo_1',['upload_slave_sdo',['../classethercat__interface_1_1EcMasterBase.html#a5c9afe492569ace54c37cb90d6701dc6',1,'ethercat_interface::EcMasterBase']]]
 ];
--- api/search/functions_d.js
+++ api/search/functions_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['read_0',['read',['../classethercat__driver_1_1EthercatBusManager.html#ad524d2f83c82df1925fbffcc9d87da06',1,'ethercat_driver::EthercatBusManager::read()'],['../classethercat__driver_1_1EthercatDriver.html#a3197ad92f1d35563cb2c3fdeb478c346',1,'ethercat_driver::EthercatDriver::read()']]],
   ['read_5fprocess_5fdata_1',['read_process_data',['../classethercat__interface_1_1EcMasterBase.html#ab846fcbfc1776999bacaa81714ce3c0b',1,'ethercat_interface::EcMasterBase']]],
-  ['readslavesdo_2',['readSlaveSdo',['../classethercat__driver_1_1EthercatBusManager.html#a59f41ec31f7588bfd0f642d5d188c9ed',1,'ethercat_driver::EthercatBusManager']]],
+  ['readslavesdo_2',['readSlaveSdo',['../classethercat__driver_1_1EthercatBusManager.html#aa3360f951e51ddb8df5b2a49cdf6b3ab',1,'ethercat_driver::EthercatBusManager']]],
   ['registerslaves_3',['registerSlaves',['../classethercat__driver_1_1EthercatBusManager.html#a27571650e5d69fb5de28954060edda64',1,'ethercat_driver::EthercatBusManager']]],
   ['registertransferindomain_4',['registerTransferInDomain',['../classethercat__interface_1_1EcMasterBase.html#a8b94755bcfd2164e46ffb98b2eb23319',1,'ethercat_interface::EcMasterBase']]],
   ['reset_5',['reset',['../classethercat__interface_1_1EcTransferNet.html#ae9457a97c0b78e86ef56e5917503354c',1,'ethercat_interface::EcTransferNet::reset()'],['../classethercat__interface_1_1EcMasterBase.html#a847fc7515be947b6a8a786c73bcdce8d',1,'ethercat_interface::EcMasterBase::reset()']]]
